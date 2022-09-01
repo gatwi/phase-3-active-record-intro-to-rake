f673c63292976130070dd0362384ad30750c8ca5
gatwi <faith.murithi@student.moringaschool.com>
2022-09-01 09:08:44 +0300

    Learning Rake

--- config/environment.rb
+++ config/environment.rb
@@ -4,3 +4,8 @@ Bundler.require
 require_relative "../lib/student"
 
 DB = { conn: SQLite3::Database.new("db/students.db") }
+
+desc 'drop into the Pry console'
+task console: :environment do
+  Pry.start
+end
